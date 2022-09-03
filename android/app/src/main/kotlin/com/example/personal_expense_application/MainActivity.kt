70e8e3be132758c92ab360423789bb6e6e168d68
Anıl Aydın <anil_aydinn@outlook.com>
2022-09-03 15:48:53 +0300

    [Anil] initial structure

---
+++ android/app/src/main/kotlin/com/example/personal_expense_application/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.personal_expense_application
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Flutter App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter App'),
+      ),
+      body: Column(
+        children: const [
+          SizedBox(
+            width: double.infinity,
+            child: Card(
+              color: Colors.blue,
+              elevation: 5,
+              child: Text('CHART!'),
+            ),
+          ),
+          Card(
+            child: Text('List of TX'),
+          ),
+        ],
+      ),
+    );
+  }
+}
