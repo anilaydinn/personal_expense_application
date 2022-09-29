8e33ef61c2fa69fa9cdc438bb01e9a8aecfb5a48
Anıl Aydın <anil_aydinn@outlook.com>
2022-09-29 23:25:20 +0300

    [Anil] Change height of tx list

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -11,7 +11,7 @@ class TransActionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 300,
+      height: 450,
       child: transactions.isEmpty
           ? Column(
               children: [
